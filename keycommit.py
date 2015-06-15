b48b7834baddd7cc15d170f3e3b68686979f8c52
AVatch <avatchinsky@outlook.com>
2015-06-15 14:22:20 -0400

    regex match on identifiers and getting all index occurances

--- keycommit.py
+++ keycommit.py
@@ -3,6 +3,7 @@ import fileinput
 from parser.reader import load_csv
 from parser.parser import scan_text
 
+BANDWIDTH = 5
 
 def init():
     # read in the commited snippet
@@ -11,11 +12,12 @@ def init():
         commit += line
 
     # load the dependency files
-    assignment_operators = load_csv("data/assignment_operators.csv")
     identifiers = load_csv("data/identifiers.csv")
+    assignment_operators = load_csv("data/assignment_operators.csv")
 
     # pass commited snippet with data
     # to parser
+    candidates = scan_text(commit, identifiers, assignment_operators, BANDWIDTH)
 
 
     # validate candidates
--- parser/parser.py
+++ parser/parser.py
@@ -1,17 +1,22 @@
 import re
 
 
-def scan_text(text):
+def scan_text(text, identifiers, operators, BANDWIDTH):
     """
     Identifies potential api keys in a text file
     """
-    # identify indecies of identifiers
-
-    # check if assignment operator is present
+    candidates = {}
+    # normalize the text
+    text = text.lower()
+    # identify indecies of identifiers 
+    for identifier, identifier_weight in identifiers.iteritems():
+        matches = [m.start() for m in re.finditer(identifier, text)]
+        if matches:
+            candidates[identifier] = matches
 
     # check if assigned value is key
-
-    return ""
+    print candidates
+    return candidates
 
 if __name__=="__main__":
     pass
